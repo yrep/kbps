91497e003751f8c8d20a47eaddf073c65c993df4
yrep <yurkfb@gmail.com>
2025-04-21 01:02:20 +0400

    Woocommerce style priority fix

--- inc/KBPSThemeAssets.php
+++ inc/KBPSThemeAssets.php
@@ -1,36 +1,15 @@
 <?php
 class KBPSThemeAssets {
     public function __construct() {
-        //add_filter('woocommerce_enqueue_styles', '__return_empty_array' );
-        //add_filter('woocommerce_enqueue_styles', '__return_empty_array');
-        //add_action('woocommerce_enqueue_styles', [$this, 'overrideWoocommerceStyles'], 20);
+        add_filter('woocommerce_enqueue_styles', '__return_empty_array');
+        //add_filter('woocommerce_should_load_block_theme_styles', '__return_false');
         add_action('wp_enqueue_scripts', [$this, 'enqueueStyles'], 100);
         add_action('wp_enqueue_scripts', [$this, 'enqueueFontAwesome']);
         add_action('wp_enqueue_scripts', [$this, 'enqueueScripts'], 101);
-        //add_action('wp_enqueue_scripts', [$this, 'enqueueMainStylesAfterWcStyles'], 800);
     }
-
-    // public function overrideWoocommerceStyles($styles) {
-    //     if (class_exists('WooCommerce')) {
-    //         wp_enqueue_style(
-    //             'kbps-woocommerce',
-    //             KBPS_THEME_DIR . 'assets/css/kbps-woocommerce.css',
-    //             ['woocommerce-general', 'woocommerce-layout', 'woocommerce-smallscreen'],
-    //             '1.0.0'
-    //         );
-    //     }
-    //     return $styles;
-    // }
 
 
     public function enqueueStyles() {
-
-        // if (class_exists('WooCommerce')) {
-        //     wp_dequeue_style('woocommerce-general');
-        //     wp_dequeue_style('woocommerce-layout');
-        //     wp_dequeue_style('woocommerce-smallscreen');
-        // }
-
 
         wp_enqueue_style(
             'kbps-core-reset',
@@ -38,32 +17,6 @@ class KBPSThemeAssets {
             array(),
             filemtime(KBPS_THEME_DIR . 'assets/css/core-reset.css')
         );
-        
-/*
-        wp_enqueue_style('woocommerce-general', plugins_url() .'/woocommerce/assets/css/woocommerce-general.css', [], WC()->version);
-        wp_enqueue_style('woocommerce-layout', plugins_url() .'/woocommerce/assets/css/woocommerce-layout.css', [], WC()->version);
-        wp_enqueue_style('woocommerce-css', plugins_url() .'/woocommerce/assets/css/woocommerce.css', [], WC()->version);
-*/
-/*
-        wp_enqueue_style(
-            'woocommerce-general',
-            plugins_url('/assets/css/woocommerce.css', WC_PLUGIN_FILE),
-            [],
-            WC()->version
-        );
-        wp_enqueue_style(
-            'woocommerce-layout',
-            plugins_url('/assets/css/woocommerce-layout.css', WC_PLUGIN_FILE),
-            ['woocommerce-general'],
-            WC()->version
-        );
-        wp_enqueue_style(
-            'woocommerce-smallscreen',
-            plugins_url('/assets/css/woocommerce-smallscreen.css', WC_PLUGIN_FILE),
-            ['woocommerce-layout'],
-            WC()->version
-        );
-*/
 
         wp_enqueue_style(
             'kbps-core-variables',
@@ -72,14 +25,17 @@ class KBPSThemeAssets {
             filemtime(KBPS_THEME_DIR . 'assets/css/core-variables.css')
         );
         
+        wp_enqueue_style('woocommerce-layout', plugins_url() .'/woocommerce/assets/css/woocommerce-layout.css', [], WC()->version);
+        wp_enqueue_style('woocommerce-smallscreen', plugins_url() .'/woocommerce/assets/css/woocommerce-smallscreen.css', ['woocommerce-layout'], WC()->version, 'only screen and (max-width: 768px)');
+        wp_enqueue_style('woocommerce-general', plugins_url() .'/woocommerce/assets/css/woocommerce.css', ['woocommerce-layout'], WC()->version);
+        wp_enqueue_style('woocommerce-block-style', plugins_url('/woocommerce/assets/css/woocommerce-blocktheme.css'), [], WC()->version);
+
         wp_enqueue_style(
             'kbps-core-typography',
             KBPS_THEME_URI . 'assets/css/core-typography.css',
             [],
             filemtime(KBPS_THEME_DIR . 'assets/css/core-typography.css')
         );
-
-        //$this->forceLoadWcStyles();
 
         wp_enqueue_style(
             'kbps-header-styles',
@@ -105,91 +61,22 @@ class KBPSThemeAssets {
         wp_enqueue_style(
             'kbps-woocommerce',
             KBPS_THEME_URI . 'assets/css/kbps-woocommerce.css',
-            ['woocommerece', 'woocommerce-layout'],
+            [],
             filemtime(KBPS_THEME_DIR . 'assets/css/kbps-woocommerce.css')
         );
         
-        /*
-        wp_register_style(
-            'kbps-woocommerce',
-            KBPS_THEME_URI . 'assets/css/kbps-woocommerce.css'
-        );
-	
-        if ( class_exists( 'woocommerce' ) ) {
-            wp_enqueue_style( 'kbps-woocommerce' );
-        }
-        */
-
-        /*
-        wp_enqueue_style(
-            'kbps-main-styles',
-            KBPS_THEME_URI . 'assets/css/main-styles.css',
-            ['woocommerce-general', 'woocommerce-layout', 'kbps-core-reset'],
-            filemtime(KBPS_THEME_DIR . 'assets/css/main-styles.css')
-        );
-        */
     }
-
-
-    /**
-     *  WooCommerce
-     */
-    /*
-    protected function forceLoadWcStyles() {
-        if (!class_exists('WooCommerce')) return;
-
-        wp_dequeue_style('woocommerce-general');
-        wp_dequeue_style('woocommerce-layout');
-        wp_dequeue_style('woocommerce-css');
-        // wp_dequeue_style('woocommerce-general');
-        // wp_dequeue_style('woocommerce-smallscreen');
-
-        wp_enqueue_style('woocommerce-general', plugins_url() .'/woocommerce/assets/css/woocommerce-general.css');
-        wp_enqueue_style('woocommerce-layout', plugins_url() .'/woocommerce/assets/css/woocommerce-layout.css');
-        wp_enqueue_style('woocommerce-css', plugins_url() .'/woocommerce/assets/css/woocommerce.css');
-        // wp_enqueue_style('woocommerce-smallscreen', plugins_url() .'/woocommerce/assets/css/woocommerce-smallscreen.css');
-        // wp_enqueue_style('woocommerce-blocktheme', plugins_url() .'/woocommerce/assets/css/woocommerce-blocktheme.css');
-        /*
-        wp_enqueue_style(
-            'woocommerce-general',
-            plugins_url('woocommerce/assets/css/woocommerce.css', WC_PLUGIN_FILE),
-            array(),
-            WC_VERSION
-        );
-        
-        wp_enqueue_style(
-            'woocommerce-layout',
-            plugins_url('woocommerce/assets/css/woocommerce-layout.css', WC_PLUGIN_FILE),
-            array('woocommerce-general'),
-            WC_VERSION
-        );
-        */
-//    }
 
 
     public function enqueueScripts() {
         wp_enqueue_script('custom-menu', KBPS_THEME_URI . '/assets/js/mobile-menu.js', array(), '1.0', true);
         wp_enqueue_script('to-top-button', KBPS_THEME_URI . '/assets/js/to-top-button.js', array(), '1.0', true);
         
-        /*
-        wp_enqueue_script(
-            'cake-order-script', 
-            KBPS_THEME_URI . '/assets/js/cake-order.js', 
-            [], 
-            '1.0', 
-            true
-        );
-
-        wp_localize_script('cake-order-script', 'cakeOrderVars', [
-            'ajax_url' => admin_url('admin-ajax.php'),
-            'nonce' => wp_create_nonce('cake_order_nonce')
-        ]);
-        */
         wp_enqueue_script(
             'kbps-cake-order',
             get_template_directory_uri() . '/assets/js/cake-order.js',
             array(),
-            '1.0.1', // Обновите версию для сброса кэша
+            '1.0.1',
             true
         );
  
